452e4e0f518f964f4aa73048bddbdb71a9030e73
Elora28 <elora.bereket2468@gmail.com>
2026-03-31 15:51:23 -0500

    Add MiniProject2

---
+++ MiniProject2.py
@@ -0,0 +1,17 @@
+import random
+
+number = random.randint(1, 100)   # pick a random number
+attempts = 0                      # start counting at 0
+guess = 0
+while guess != number:
+    guess = int(input("Enter your guess: "))
+    attempts = attempts + 1       # add 1 each time
+
+    if guess > number:
+        print("Too high")
+    elif guess < number:
+        print("Too low")
+    else:
+        print("Correct!")
+
+print("You used", attempts, "attempts")
